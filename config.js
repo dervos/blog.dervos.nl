27ca7ea3a570efa75c1fc05bdebbe93513fcfdb3
Reinhard van Apeldoorn <reinhard@dervos.nl>
2016-11-14 12:43:30 +0100

    fixed host env var

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ config = {
         database: {
             client: 'mysql',
             connection: {
-                host     : process.env.DB_PORT,
+                host     : process.env.DB_HOST,
                 user     : process.env.DB_USER,
                 password : process.env.DB_PASSWORD,
                 database : process.env.DB_NAME,
